d994297365b78192f076d4c73c725366eefbf2a3
uFrost <Jcub142@gmail.com>
2015-02-02 17:24:35 -0800

    Updates to UI and Window framework alterations

--- src/Renderer/Window.java
+++ src/Renderer/Window.java
@@ -1,21 +1,21 @@
 package Renderer;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.Panel;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.util.ArrayList;
-import java.util.Collections;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
 import Engine.GameObject;
 
 
-//TODO: 
+
 
 public class Window extends PixDumpWindow{
 
@@ -26,9 +26,11 @@ public class Window extends PixDumpWindow{
 	
 	Window(ArrayList<GameObject> objects) {
 		super();
+		this.setTitle("Pixel Dump Development Build! (get to work already)");
 		this.setLayout(new GridLayout(1,2));
 		this.setPreferredSize(new Dimension(700,400));
 		this.setResizable(false);
+		
 		
 		JPanel InspEiarchy = new JPanel();
 		InspEiarchy.setLayout(new GridLayout(1,4));
@@ -48,21 +50,35 @@ public class Window extends PixDumpWindow{
 	
 	//This is the "View" side of createObjArray from the "Object" class. It literally just lists the objects
 	JPanel UpdateHeiarchy(){
+		
 	ArrayList<GameObject> objects= GameObject.getAllGameObjects();
+	
 		JPanel panel = new JPanel();
-		//panel.setLayout(null);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 		
 		//adds to heiarchy UI
+		panel.add(makeText("Heiarchy:"));
+		panel.add(Box.createRigidArea(new Dimension(0,2)));
+		
+		
+		
 		for(int x = 0; x<objects.size(); x++){
 
-			panel.add(makeText(objects.get(x).name()));
+			panel.add(makeText("     "+objects.get(x).name()));
+			panel.add(Box.createRigidArea(new Dimension(0,2)));
 			System.out.println(objects.get(x).name());
+			
 			if(objects.get(x).GetChildren()!=null){
 				for(int y = 0; y<objects.get(x).GetChildren().size(); y++){
-				panel.add(makeText(objects.get(x).GetChildren().get(y).name()));
-				System.out.println("Child: " + objects.get(x).GetChildren().get(y).name());
+					
+					objects.remove(objects.get(x).GetChildren().get(y));
+					panel.add(makeText("                 "+objects.get(x).GetChildren().get(y).name()));
+					panel.add(Box.createRigidArea(new Dimension(0,2)));
+					System.out.println("Child: " + objects.get(x).GetChildren().get(y).name());
+					
 				}
 			}
+			
 		}
 		return panel;
 	}
@@ -77,10 +93,15 @@ public class Window extends PixDumpWindow{
 	
 	
 	
-	JTextArea makeText(String name){
-		final JTextArea textArea = new JTextArea();
-		textArea.setText(name);
-        textArea.addFocusListener(new FocusListener() {
+	JLabel makeText(String name){
+		
+		final JLabel text = new JLabel();
+		text.setText(name);
+		text.setMinimumSize(new Dimension(10,10));
+		text.setPreferredSize(new Dimension(10,10));
+		text.setMaximumSize(new Dimension(Short.MAX_VALUE,20));
+		text.setForeground(new Color(150, 150, 150));
+        text.addFocusListener(new FocusListener() {
 
             @Override
             public void focusGained(FocusEvent e) {
@@ -92,7 +113,7 @@ public class Window extends PixDumpWindow{
                 //Your code here
             }
         });
-        return textArea;
+        return text;
 	}
 	
 }
