5768ad7854ccd5a65556aa6bcb5c32cc02d129d7
uFrost <Jcub142@gmail.com>
2015-02-25 12:13:07 -0800

    Added PreSpector

--- src/Renderer/UI.java
+++ src/Renderer/UI.java
@@ -1,7 +1,11 @@
 package Renderer;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.util.ArrayList;
+
+import javax.swing.JLabel;
 
 import Engine.GameObject;
 
@@ -21,4 +25,17 @@ public class UI   {
 		
 	}
 	
+	public static JLabel MakeText(String text) {
+
+		JLabel p = new JLabel();
+
+		p.setText(text);
+		p.setMinimumSize(new Dimension(10, 10));
+		p.setPreferredSize(new Dimension(10, 10));
+		p.setMaximumSize(new Dimension(Short.MAX_VALUE, 20));
+		p.setForeground(new Color(150, 150, 150));
+
+		// keep this as -1 otherwise textCount++ ain't gonna go through
+		return p;
+	}
 }
