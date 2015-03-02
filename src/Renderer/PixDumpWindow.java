b9506c81433b2be606d8b2d5f51e6ddcbb9dcb9a
uFrost <Jcub142@gmail.com>
2015-03-01 16:08:08 -0800

    Gui color changes

--- src/Renderer/PixDumpWindow.java
+++ src/Renderer/PixDumpWindow.java
@@ -24,14 +24,18 @@ public class PixDumpWindow extends JFrame {
 			Properties props = new Properties();
                         
             props.put("selectionBackgroundColor", "50 50 50"); 
-            props.put("menuSelectionBackgroundColor", "10 10 10"); 
+            props.put("selectionForegroundColor", "100 100 100");
             
+            props.put("menuSelectionBackgroundColor", "30 30 30"); 
+            props.put("menuSelectionForegroundColor", "150 150 150");
+            props.put("menuBackgroundColor", "10 10 10"); 
             
             props.put("windowInactiveTitleColorLight", "20 20 20"); 
             props.put("windowInactiveTitleColorDark", "0 0 0");
             props.put("windowInactiveBorderColor", "0 0 0");
             
             props.put("backgroundColor", "20 20 20");
+            props.put("foregroundColor", "100 100 100");
             
             props.put("controlColor", "10 10 10");
             props.put("controlColorLight", "10 10 10");
@@ -40,12 +44,25 @@ public class PixDumpWindow extends JFrame {
             props.put("buttonColor", "10 10 10");
             props.put("buttonColorLight", "10 10 10");
             props.put("buttonColorDark", "10 10 10");
+            
+            props.put("toolbarBackgroundColor", "10 10 10");
+            props.put("toolbarBackgroundColor", "150 150 150");
 
             props.put("rolloverColor", "50 50 50"); 
             props.put("rolloverColorLight", "50 50 50"); 
-            props.put("rolloverColorDark", "10 10 10"); 
+            props.put("rolloverColorDark", "10 10 10");
+            
+            props.put("focusColor", "40 40 40"); 
             
             props.put("frameColor", "100 100 100");
+            
+            props.put("menuColorLight", "30 30 30");
+            props.put("menuColorDark", "10 10 10");
+            
+            props.put("inputForegroundColor", "150 150 150");
+            props.put("inputBackgroundColor", "40 40 40");
+            
+            props.put("logoString", "");
             
             props.put("windowInactiveTitleForegroundColor", "100 100 100");
             props.put("windowTitleForegroundColor", "200 200 200");
--- src/Utils/windowMenu.java
+++ src/Utils/windowMenu.java
@@ -1,5 +1,6 @@
 package Utils;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 
@@ -12,18 +13,49 @@ public class windowMenu extends JMenuBar{
 
 	JMenu File = new JMenu();
 	JMenu GameObjectMenu = new JMenu();
-	JMenuItem menuItem = new JMenuItem("Save",
+	
+	JMenuItem Save = new JMenuItem("Save Scene",
+            KeyEvent.VK_T);
+	JMenuItem NewScene = new JMenuItem("New Scene",
+            KeyEvent.VK_T);
+	JMenuItem NewProject = new JMenuItem("New Project",
+            KeyEvent.VK_T);
+	JMenuItem Build = new JMenuItem("Build",
+            KeyEvent.VK_T);
+	
+	JMenuItem NewGameObject = new JMenuItem("New Game Object",
             KeyEvent.VK_T);
 	
 	public windowMenu(){
 		
 		this.add(Box.createRigidArea(new Dimension(0,10)));
 		
+		File.setForeground(new Color(150,150,150));
+		GameObjectMenu.setForeground(new Color(150,150,150));
+		
+		Save.setForeground(new Color(150,150,150));
+		NewScene.setForeground(new Color(150,150,150));
+		NewProject.setForeground(new Color(150,150,150));
+		Build.setForeground(new Color(150,150,150));
+		NewGameObject.setForeground(new Color(150,150,150));
+		
+		
 		File.setText("File");
-		File.add(menuItem);
-		File.addSeparator();
+			File.add(Save);
+			File.addSeparator();
+			
+			File.add(NewScene);
+			File.addSeparator();
+			
+			File.add(NewProject);
+			File.addSeparator();
+			
+			File.add(Build);
+			File.addSeparator();
 		
 		GameObjectMenu.setText("Game Object");
+			GameObjectMenu.add(NewGameObject);
+			GameObjectMenu.addSeparator();
 
 		this.add(File);
 		this.add(GameObjectMenu);
